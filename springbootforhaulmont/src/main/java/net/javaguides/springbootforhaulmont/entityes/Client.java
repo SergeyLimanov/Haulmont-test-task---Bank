9ff0cd3ecedcec048ffc9146a731ac4267834b1a
дом <imajing2010@yandex.ru>
2021-06-24 01:33:05 +0400

    middle name

--- springbootforhaulmont/src/main/java/net/javaguides/springbootforhaulmont/entityes/Client.java
+++ springbootforhaulmont/src/main/java/net/javaguides/springbootforhaulmont/entityes/Client.java
@@ -19,6 +19,9 @@ import java.util.UUID;
     @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "middle_name")
+    private String middleName;
+
     @Column(name = "last_name")
     private String lastName;
 
