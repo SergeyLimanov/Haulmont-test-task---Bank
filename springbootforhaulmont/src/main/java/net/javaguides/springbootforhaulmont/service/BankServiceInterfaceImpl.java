721ad940dcb5bb94af057d3ee6c898d4c5a66111
SergeyLimanov <imajing2010@yandex.ru>
2021-07-01 16:05:01 +0300

    попробовать запустить с пустой базой

--- springbootforhaulmont/src/main/java/net/javaguides/springbootforhaulmont/service/BankServiceInterfaceImpl.java
+++ springbootforhaulmont/src/main/java/net/javaguides/springbootforhaulmont/service/BankServiceInterfaceImpl.java
@@ -3,6 +3,7 @@ package net.javaguides.springbootforhaulmont.service;
 import net.javaguides.springbootforhaulmont.model.Bank;
 import net.javaguides.springbootforhaulmont.repository.BankRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,6 +19,7 @@ public class BankServiceInterfaceImpl implements BankServiceInterface {
         this.bankRepository = bankRepository;
     }
 
+    @Nullable
     @Override
     public Bank findBankById(UUID id) {
         return bankRepository.findById(id).orElse(null);
