c82c3224fa5fc0777b1c263d177de807b62cad0c
Vladislav Gruzdov <Gruzdov.V.1991@Hotmail.com>
2021-07-02 23:40:27 +0400

    message

---
+++ springbootforhaulmont/target/classes/db/addition.sql
@@ -0,0 +1,37 @@
+INSERT INTO bank(BANK_ID, BANK_NAME)
+
+VALUES('39574515-7129-4ac7-90ad-19e063b255c1','first'),
+<<<<<<< HEAD
+      ('34574515-7129-4ac7-90ad-19e063b255c2','second');
+=======
+    ('34574515-7129-4ac7-90ad-19e063b255c2','second');
+>>>>>>> origin/main
+
+INSERT INTO credit(CREDIT_ID, CREDIT_PERCENTAGE, LIMIT_OF_MONEY, TYPE_OF_CREDIT, BANK_ID)
+
+VALUES ('15574515-7129-4ac7-90ad-19e063b255c1', 1, 22, 'okoko', '39574515-7129-4ac7-90ad-19e063b255c1'),
+       ('25574515-7129-4ac7-90ad-19e063b255c1', 2, 33, 'kk', '34574515-7129-4ac7-90ad-19e063b255c2');
+
+INSERT INTO client(client_id, email, first_name, last_name, middle_name, passport_number, phone_number, bank_id)
+<<<<<<< HEAD
+VALUES('15574515-7133-4ac7-90ad-19e063b255c1', 'mail', 'ser', 'mer', 'per', 'sd3232w', 8999,'39574515-7129-4ac7-90ad-19e063b255c1'),
+      ('15574515-7134-4ac7-90ad-19e063b255c1', 'mail.ru', 'sergey', 'viktorovich', 'loimanov', 'sd3232w', 898899,'34574515-7129-4ac7-90ad-19e063b255c2');
+
+INSERT INTO offer_of_credit(offer_of_credit_id, credit_term, first_payment, name_of_credit, sum, sum_of_percent, take_date_of_credit, bank_id, client_id, credit_id)
+VALUES ('35574515-7129-4ac8-90ad-19e063b255c2', 5, 222, 'ipoteca', 12222, 2, DATE '2021-01-01', '39574515-7129-4ac7-90ad-19e063b255c1', '15574515-7133-4ac7-90ad-19e063b255c1', '15574515-7129-4ac7-90ad-19e063b255c1');
+
+INSERT INTO schedule_of_payment(schedule_of_payment_id, date_of_payment, amount_of_payment, amount_of_payment_per_body, amount_of_repayment_percent, repayment_remains, offer_of_credit_id)
+
+VALUES('35574515-7129-4ac8-91ad-19e063b255c2', DATE '2021-09-01', 11, 2, 1, 13, '35574515-7129-4ac8-90ad-19e063b255c2')
+=======
+    VALUES('15574515-7133-4ac7-90ad-19e063b255c1', 'mail', 'ser', 'mer', 'per', 'sd3232w', 8999,'39574515-7129-4ac7-90ad-19e063b255c1'),
+          ('15574515-7134-4ac7-90ad-19e063b255c1', 'mail.ru', 'sergey', 'viktorovich', 'loimanov', 'sd3232w', 898899,'34574515-7129-4ac7-90ad-19e063b255c2');
+
+INSERT INTO offer_of_credit(offer_of_credit_id, credit_term, first_payment, name_of_credit, sum, sum_of_percent, take_date_of_credit, bank_id, client_id, credit_id)
+    VALUES ('35574515-7129-4ac8-90ad-19e063b255c2', 5, 222, 'ipoteca', 12222, 2, 'may', '39574515-7129-4ac7-90ad-19e063b255c1', '15574515-7133-4ac7-90ad-19e063b255c1', '15574515-7129-4ac7-90ad-19e063b255c1');
+
+INSERT INTO schedule_of_payment(schedule_of_payment_id, amount_of_payment, amount_of_payment_per_body, amount_of_repayment_percent, date_of_payment, amount_of_repayment_remains, offer_of_credit_id, repayment_remains)
+  VALUES('35574515-7129-4ac8-91ad-19e063b255c2', 22222, 11, 2, DATE '2021-09-01', 23, '15574515-7129-4ac7-90ad-19e063b255c1', 38 )
+>>>>>>> origin/main
+
+
---
+++ springbootforhaulmont/target/classes/db/initializationDB.sql
@@ -0,0 +1,59 @@
+DROP TABLE Schedule_of_payment IF EXISTS CASCADE;
+DROP TABLE Offer_of_credit IF EXISTS CASCADE;
+DROP TABLE Credit IF EXISTS;
+DROP TABLE Client IF EXISTS;
+DROP TABLE Bank IF EXISTS;
+
+CREATE TABLE Bank (
+                      bank_id uuid not null,
+                      bank_name varchar not null,
+                      PRIMARY KEY (bank_id)
+);
+
+CREATE TABLE Client (
+                        client_id uuid not null,
+                        first_name varchar not null,
+                        last_name varchar not null,
+                        phone_number varchar not null,
+                        email varchar not null,
+                        passport_number varchar not null,
+                        PRIMARY KEY (client_id),
+                        bank_id uuid
+
+
+);
+
+create table Credit (
+                        credit_id uuid not null,
+                        limit_of_money numeric not null,
+                        credit_percentage numeric not null,
+                        bank_id uuid,
+                        type_of_credit varchar not null,
+                        PRIMARY KEY (credit_id)
+
+);
+
+create table Offer_of_credit (
+                              offer_of_credit_id uuid not null,
+                              Client_id uuid,
+                              Credit_id uuid,
+                              name_of_credit varchar not null,
+                              sum_of_percent numeric not null,
+                              first_payment numeric not null,
+                              credit_term int not null,
+                              Bank_id uuid,
+                              take_date_of_credit date not null,
+                              PRIMARY KEY (offer_of_credit_id)
+
+);
+
+create table Schedule_of_payment (
+                                  schedule_of_payment_id uuid not null,
+                                  date_of_payment date not null,
+                                  amount_of_payment numeric not null,
+                                  amount_of_payment_per_body numeric not null,
+                                  amount_of_repayment_percent numeric not null,
+                                  offer_of_credit_id uuid,
+                                  PRIMARY KEY (schedule_of_payment_id)
+
+);
