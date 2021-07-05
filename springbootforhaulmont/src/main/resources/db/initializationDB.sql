dfdaee2003f7c4028ce1933455c31d9b1fb72e7e
SergeyLimanov <imajing2010@yandex.ru>
2021-07-05 15:39:24 +0300

    NULL solved

--- springbootforhaulmont/src/main/resources/db/initializationDB.sql
+++ springbootforhaulmont/src/main/resources/db/initializationDB.sql
@@ -8,9 +8,9 @@ CREATE TABLE Bank (
 
 create table Credit (
                         credit_id uuid not null,
-                        type_of_credit varchar not null,
-                        limit_of_money numeric not null,
-                        credit_percentage numeric not null,
+                        type_of_credit varchar,
+                        limit_of_money numeric,
+                        credit_percentage numeric,
                         bank_id uuid,
                         PRIMARY KEY (credit_id)
 
@@ -18,12 +18,12 @@ create table Credit (
 
 CREATE TABLE Client (
                         client_id uuid not null,
-                        first_name varchar not null,
-                        middle_name varchar not null,
-                        last_name varchar not null,
-                        phone_number varchar not null,
-                        email varchar not null,
-                        passport_number varchar not null,
+                        first_name varchar,
+                        middle_name varchar,
+                        last_name varchar,
+                        phone_number varchar,
+                        email varchar,
+                        passport_number varchar,
                         bank_id uuid,
                         PRIMARY KEY (client_id)
 
@@ -32,27 +32,27 @@ CREATE TABLE Client (
 
 
 create table Offer_of_credit (
-                              offer_of_credit_id uuid not null,
-                              name_of_credit varchar not null,
-                              sum numeric  not null,
-                              credit_term int not null,
-                              first_payment numeric not null,
-                              sum_of_percent numeric not null,
-                              Bank_id uuid,
-                              Client_id uuid,
-                              Credit_id uuid,
-                              PRIMARY KEY (offer_of_credit_id)
+                                 offer_of_credit_id uuid not null,
+                                 name_of_credit varchar,
+                                 sum numeric,
+                                 credit_term int,
+                                 first_payment numeric,
+                                 sum_of_percent numeric,
+                                 Bank_id uuid,
+                                 Client_id uuid,
+                                 Credit_id uuid,
+                                 PRIMARY KEY (offer_of_credit_id)
 
 );
 
 create table Schedule_of_payment (
-                                  schedule_of_payment_id uuid not null,
-                                  amount_of_payment_per_body numeric not null,
-                                  amount_of_repayment_percent numeric not null,
-                                  amount_of_payment numeric not null,
-                                  date_of_payment date not null,
-                                  remains numeric not null,
-                                  offer_of_credit_id uuid,
-                                  PRIMARY KEY (schedule_of_payment_id)
+                                     schedule_of_payment_id uuid not null,
+                                     amount_of_payment_per_body numeric,
+                                     amount_of_repayment_percent numeric,
+                                     amount_of_payment numeric,
+                                     date_of_payment date,
+                                     remains numeric,
+                                     offer_of_credit_id uuid,
+                                     PRIMARY KEY (schedule_of_payment_id)
 
 );
